ceeebc11aebabf15bce138e2ef44fed6b585d09c
sjenson <sjenson@oberlin.edu>
2018-06-30 15:43:08 +0200

    In progress, resetting computer

--- fungal_growth.cpp
+++ fungal_growth.cpp
@@ -1,4 +1,4 @@
-//#include "kdtree.h"
+#include "kdtree.h"
 
 #include <iostream>
 #include <array>
@@ -12,11 +12,9 @@ using namespace emscripten;
 using Point = Eigen::Vector3d;
 using Edge = std::pair<int, int>;
 using Segment = std::pair<Point, Point>;
+using SimplePoint = std::array<float, 3>;
 
-struct Point3f
-{
-    float x, y, z;
-};
+using Tree = jk::tree::KDTree<Point, 3>;
 
 struct Hypha
 {
@@ -25,6 +23,24 @@ struct Hypha
     Eigen::Quaterniond orientation;
 };
 
+SimplePoint makeSimplePoint(const Point& point)
+{
+    SimplePoint p;
+    p[0] = point.x();
+    p[1] = point.y();
+    p[2] = point.z();
+    return p;
+}
+
+Point makePoint(const SimplePoint& point)
+{
+    Point p;
+    p.x() = point[0];
+    p.y() = point[1];
+    p.z() = point[2];
+    return p;
+}
+
 class Fungus
 {
 public:
@@ -32,17 +48,17 @@ public:
     {
         Hypha h1, h2, h3;
         h1.position = {1, 0, 0};
-        h1.velocity = {1, 0, 0};
-        h2.position = {0, 1, 0};
-        h2.velocity = {0, 1, 0};
-        h3.position = {-1, 0, 0};
-        h3.velocity = {-1, 0, 0};
+        h1.velocity = {.1, 0, 0};
+        h2.position = {0, .1, 0};
+        h2.velocity = {0, .1, 0};
+        h3.position = {-.1, 0, 0};
+        h3.velocity = {-.1, 0, 0};
         tips.push_back(h1);
         tips.push_back(h2);
         tips.push_back(h3);
     }
 
-    void update()
+    void newTips()
     {
         for (auto& tip : tips)
         {
@@ -51,16 +67,56 @@ public:
         }
     }
 
-    std::vector<Point3f> getNewPoints()
+    void collisionAvoidance()
     {
-        std::vector<Point3f> simplePoints;
+        Tree tree;
+        bool autoSplit{false};
+        for (const auto& point : points)
+        {
+            tree.addPoint(makeSimplePoint(point), 0.f, autoSplit);
+        }
+        tree.splitOutstanding();
+
+        std::vector<Point> offsets(points.size(), Point::Zeros());
+
+        size_t idx{0};
+        double radius{10};
+        for (const auto& point : points)
+        {
+            auto nearPoints = tree.searchBall(makeSimplePoint(point), radius * radius);
+
+            for (const auto& nearPoint : nearPoints)
+            {
+                Point otherPoint = makePoint(nearPoint);
+                Point offset = otherPoint - point;
+                double distance = Eigen::squaredNorm(offset);
+
+                offsets[idx] += offset * (1.0 / std::max(0.1, distance));
+            }
+            idx++;
+        }
+
+        size_t idx{0}
+        for (const auto& point : points)
+        {
+            point += offsets[idx];
+            idx++;
+        }
+    }
+
+    void update()
+    {
+        newTips();
+        collisionAvoidance();
+    }
+
+    std::vector<SimplePoint> getNewPoints()
+    {
+        std::vector<SimplePoint> simplePoints;
 		simplePoints.reserve(points.size());
         for (const auto& point : points)
         {
-            Point3f p;
-            p.x = point.x();
-            p.y = point.y();
-            p.z = point.z();
+            SimplePoint p = makeSimplePoint(point);
             simplePoints.push_back(p);
         }
 		points.clear();
@@ -77,12 +133,12 @@ private:
 
 EMSCRIPTEN_BINDINGS(fungus)
 {
-    value_array<Point3f>("Point")
-        .element(&Point3f::x)
-        .element(&Point3f::y)
-        .element(&Point3f::z);
+    value_array<SimplePoint>("Point")
+        .element(&SimplePoint[0])
+        .element(&SimplePoint[1])
+        .element(&SimplePoint[2]);
 
-    register_vector<Point3f>("VectorPoint");
+    register_vector<SimplePoint>("VectorPoint");
 
     class_<Fungus>("Fungus")
         .constructor<>()
